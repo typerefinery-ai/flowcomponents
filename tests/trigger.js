0efe5ca1b2010c64e5c9a70a2afea72062ecd081
wildone <max.barrass@gmail.com>
2023-01-31 13:33:15 +1100

    update test for trigger.

--- tests/trigger.js
+++ tests/trigger.js
@@ -17,7 +17,7 @@ require('../tester')(async function(describe, done) {
 	await describe(function(test, next) {
 
 		// Random string
-		test.configure({ random: true, data: 'NOT_RANDOM' });
+		test.configure({ random: true, data: 'NOT_RANDOM', type: 'string' });
 		test.output = function(msg) {
 			test.fail(msg.data === 'NOT_RANDOM', 'Random string');
 		};
@@ -26,7 +26,7 @@ require('../tester')(async function(describe, done) {
 
 		// NOT Random string
 		setTimeout(function() {
-			test.configure({ random: false, data: 'NOT_RANDOM' });
+			test.configure({ random: false, data: 'NOT_RANDOM', type: 'string' });
 
 			test.output = function(msg) {
 				test.ok(msg.data === 'NOT_RANDOM', 'NOT Random string');
